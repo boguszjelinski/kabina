66d398382703aa425c4403f96d7407c2216ef473
kabina <bogusz.jelinski@gmail.com>
2022-01-01 21:50:12 +0100

    distance is shown

--- src/components/Kabina/Kabina.tsx
+++ src/components/Kabina/Kabina.tsx
@@ -30,8 +30,9 @@ export const Kabina: FunctionComponent = () => {
     }
 
     const columns = [
-        { title: 'Stop', field: 'stop'},
+        { title: 'Stop', field: 'stop', width: '40%'},
         { title: 'Status', field: 'status'},
+        { title: 'Distance to', field: 'distance'}
     ];
 
     const orderColumns = [
@@ -42,7 +43,7 @@ export const Kabina: FunctionComponent = () => {
         { title: 'Max wait', field: 'maxWait'},
         { title: 'Shared', field: 'shared'},
         { title: 'In pool', field: 'inPool'},
-        { title: 'Received', field: 'rcvdTime'},
+        { title: 'Received', field: 'received'},
         { title: 'At time', field: 'atTime'},
         { title: 'ETA', field: 'eta'},
         { title: 'Cab', field: 'cab'},
@@ -67,23 +68,30 @@ export const Kabina: FunctionComponent = () => {
             let rowArr: TripLeg[] = [];
             if (order.route && order.route.status !== 'COMPLETED') { 
                 let firstFound = false;
+                let distance = 0;
                 for (let leg of order.route.legs) {
                     // find the first leg for this customer
                     if (!firstFound && leg.fromStand === order.fromStand) {
                         firstFound = true;
-                    }
-                    if (firstFound) {
                         rowArr.push(new TripLeg(
                             stopName(leg.fromStand),
                             explain(leg.status),
                             0
                         ));
+                        distance = leg.distance;
+                    } else if (firstFound) {
+                        rowArr.push(new TripLeg(
+                            stopName(leg.fromStand),
+                            explain(leg.status),
+                            distance === 0 ? 1 : distance
+                        ));
+                        distance = leg.distance; // we need to show the distance of the previous leg
                     }
                     if (leg.toStand === order.toStand) {
                         rowArr.push(new TripLeg(
                             stopName(leg.toStand),
                             explain(leg.status),
-                            0
+                            leg.distance === 0 ? 1 : leg.distance
                         ));
                         break;
                     }
@@ -93,13 +101,15 @@ export const Kabina: FunctionComponent = () => {
                 new TripOrder(
                     order.id, 
                     order.status,
-                    stopName(order.fromStand),
-                    stopName(order.toStand),
+                    order.fromStand,
+                    order.toStand,
+                    order.distance,
                     order.maxWait,
                     order.maxLoss,
                     order.shared,
-                    order.rcvdTime && order.rcvdTime.toString().length > 19
-                         ? order.rcvdTime.toString().replaceAll('T',' ').substring(0, 19) : "",
+                    trimDate(order.received),
+                    trimDate(order.started),
+                    trimDate(order.completed),
                     order.atTime,
                     order.eta,
                     order.inPool,
@@ -111,6 +121,11 @@ export const Kabina: FunctionComponent = () => {
             ));
         }
         return orderArr;
+    }
+
+    function trimDate(date: Date) : string {
+        return date && date.toString().length > 19
+            ? date.toString().replaceAll('T',' ').substring(0, 19) : "";
     }
 
     function explain(status: string) {
@@ -156,21 +171,48 @@ export const Kabina: FunctionComponent = () => {
 
     function renderHeader(order: TripOrder) {
       return ( 
-        <table>
-            <tr><th align='right'>Order ID: </th> <td align='left'>{order.id}</td></tr>
-            <tr><th align='right'>Status: </th>   <td align='left'>{order.status}</td></tr>
-            <tr><th align='right'>From: </th>     <td align='left'>{order.fromStand}</td></tr>
-            <tr><th align='right'>To: </th>       <td align='left'>{order.toStand}</td></tr>
-            <tr><th align='right'>Max wait: </th> <td align='left'>{order.maxWait}</td></tr>
-            <tr><th align='right'>Shared: </th>   <td align='left'>{order.shared?"YES":"NO"}</td></tr>
-            <tr><th align='right'>In pool: </th>  <td align='left'>{order.inPool}</td></tr>
-            <tr><th align='right'>Received: </th> <td align='left'>{order.rcvdTime}</td></tr>
-            <tr><th align='right'>At time: </th>  <td align='left'>{order.atTime}</td></tr>
-            <tr><th align='right'>ETA: </th>      <td align='left'>{order.eta}</td></tr>
-            <tr><th align='right'>Cab: </th>      <td align='left'>{order.cab}</td></tr>
-            <tr><th align='right'>Route: </th>    <td align='left'>{order.route ? order.route.id : ""}</td></tr>
+        <table style={{ padding: '0px' }} >
+            <tr><th align='right'>Order ID : </th> <td align='left'>{order.id}</td></tr>
+            <tr><th align='right'>Status : </th>   <td align='left'>{order.status}</td></tr>
+            <tr><th align='right'>From : </th>     <td align='left'>{stopName(order.fromStand)}</td></tr>
+            <tr><th align='right'>To : </th>       <td align='left'>{stopName(order.toStand)}</td></tr>
+            <tr><th align='right'>Distance : </th> <td align='left'>{order.distance}</td></tr>
+            <tr><th align='right'>Dist. with pool : </th>
+                <td align='left'>{countDistance(order)}</td></tr>
+            <tr><th align='right'>Max wait : </th> <td align='left'>{order.maxWait}</td></tr>
+            <tr><th align='right'>Shared : </th>   <td align='left'>{order.shared?"YES":"NO"}</td></tr>
+            <tr><th align='right'>In pool : </th>  <td align='left'>{order.inPool}</td></tr>
+            <tr><th align='right'>Received : </th> <td align='left'>{order.received}</td></tr>
+            <tr><th align='right'>Started : </th>  <td align='left'>{order.started}</td></tr>
+            <tr><th align='right'>At time : </th>  <td align='left'>{order.atTime}</td></tr>
+            <tr><th align='right'>ETA : </th>      <td align='left'>{order.eta}</td></tr>
+            <tr><th align='right'>Cab : </th>      <td align='left'>{order.cab}</td></tr>
+            <tr><th align='right'>Route : </th>    <td align='left'>{order.route ? order.route.id : ""}</td></tr>
         </table>
       );
+    }
+
+    function countDistance(order: TripOrder) : number {
+        let foundStart = false;
+        let sum = 0;
+        let from = order.fromStand;
+        let to = order.toStand;
+        if (!order.route || !order.route.legs) {
+            return -1; // serious error, no legs
+        }
+        for (const l of order.route.legs) {
+            if (from === l.fromStand) {
+                foundStart = true;
+                sum += (l.distance === 0 ? 1 : l.distance) + 1; // one minute (km) on the stop
+            } 
+            if (foundStart && from !== l.fromStand) { // consecutive leg 
+                sum += (l.distance === 0 ? 1 : l.distance) + 1;
+            }
+            if (to === l.toStand) { // it might be the same leg as above
+                return sum - 1; //the time spent on the last stop does not count
+            }
+        }
+        return -2; // some serious error - noe last leg
     }
 
     const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -199,11 +241,12 @@ export const Kabina: FunctionComponent = () => {
                 }
                 { item && item.legs && item.legs.length > 0
                   ? <MaterialTable columns={columns} data={item.legs} 
-                    options={{ search: false,  
-                            paging: false, 
-                            showEmptyDataSourceMessage: false, 
-                            showTitle: false
-                            }}/>
+                        options={{ search: false, paging: false, toolbar:false,
+                                showEmptyDataSourceMessage: false, 
+                                showTitle: false,
+                                headerStyle:{ fontWeight: 'bolder', fontSize: 'larger' } 
+                                }}
+                    />
                   : <></>
                 }
             </>
@@ -383,12 +426,15 @@ class TripLeg {
 class TripOrder {
     id: number;
     status: string;
-    fromStand: string;
-    toStand: string;
+    fromStand: number;
+    toStand: number;
+    distance: number;
     maxWait: number;
     maxLoss: number;
     shared: boolean;
-    rcvdTime: string;
+    received: string;
+    started: string;
+    completed: string;
     atTime?: Date;
     eta?: number;
     inPool: boolean;
@@ -399,12 +445,15 @@ class TripOrder {
     constructor(
         id: number,
         status: string,
-        fromStand: string,
-        toStand: string,
+        fromStand: number,
+        toStand: number,
+        distance: number,
         maxWait: number,
         maxLoss: number,
         shared: boolean,
-        rcvdTime: string,
+        received: string,
+        started: string,
+        completed: string,
         atTime: Date,
         eta: number,
         inPool: boolean,
@@ -418,7 +467,7 @@ class TripOrder {
             this.maxWait = maxWait;
             this.maxLoss = maxLoss;
             this.shared = shared;
-            this.rcvdTime = rcvdTime;
+            this.received = received;
             this.atTime = atTime;
             this.eta = eta > -1 ? eta : undefined;
             this.inPool = inPool;
@@ -426,6 +475,9 @@ class TripOrder {
             this.cab = cab && cab.name ? cab.name : cabId;
             this.leg = leg;
             this.route = route;
+            this.distance = distance;
+            this.started = started;
+            this.completed = completed;
     }
 }
 export interface Order {
@@ -433,10 +485,13 @@ export interface Order {
     status: string;
     fromStand: number;
     toStand: number;
+    distance: number
     maxWait: number;
     maxLoss: number;
     shared: boolean;
-    rcvdTime: Date;
+    received: Date;
+    started: Date;
+    completed: Date;
     atTime?: any;
     eta: number;
     inPool: boolean;
@@ -461,6 +516,7 @@ export interface Leg {
     id: number;
     fromStand: number;
     toStand: number;
+    distance: number;
     place: number;
     status: string;
     started: Date;
